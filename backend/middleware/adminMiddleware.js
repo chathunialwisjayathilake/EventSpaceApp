60ff64975cb0661ca194c6e85591e3636a9ec9ad
IT24101007 <IT24101007@my.sliit.lk>
2026-05-02 14:07:21 +0530

    Upload booking part only

--- backend/middleware/adminMiddleware.js
+++
@@ -1,8 +0,0 @@
-const admin = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
-    return next();
-  }
-  return res.status(403).json({ message: 'Admin access required' });
-};
-
-module.exports = { admin };
