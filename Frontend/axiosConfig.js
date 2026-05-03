aae344399e824760470fb6ed8e0882be0a4b79f8
Kithsilu <induwarakithsilu369@gmail.com>
2026-05-03 18:03:21 +0530

    Add files via upload

---
+++ Frontend/axiosConfig.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
+
+const extra =
+  Constants.expoConfig?.extra ||
+  Constants.manifest?.extra ||
+  {};
+
+export const API_URL =
+  //extra.API_URL || process.env.EXPO_PUBLIC_API_URL || 'http://192.168.1.8:5000/api';
+  extra.API_URL || process.env.EXPO_PUBLIC_API_URL || 'http://192.168.8.138:5000/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  timeout: 20000,
+});
+
+api.interceptors.request.use(async (config) => {
+  try {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch {
+    // ignore storage read errors
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message =
+      error.response?.data?.message ||
+      error.message ||
+      'Network error. Please try again.';
+    return Promise.reject(new Error(message));
+  }
+);
+
+export default api;
